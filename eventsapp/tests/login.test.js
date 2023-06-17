1f5b90b3d2a9c56b6005fdad50af248db0f40832
Dawid Cichy <dawidcichy34@gmail.com>
2023-06-17 12:00:12 +0200

    Add:Tests

---
+++ eventsapp/tests/login.test.js
@@ -0,0 +1,74 @@
+import React from "react";
+import {
+  render,
+  fireEvent,
+  queryByText,
+  screen,
+} from "@testing-library/react-native";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import "@testing-library/jest-dom";
+
+import Login from "../pages/login";
+
+const mockStore = configureStore([]);
+
+test("inavlid emai;", () => {
+  const store = mockStore({});
+
+  const { getByPlaceholderText, getByText } = render(
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  );
+
+  const emailInput = getByPlaceholderText("email");
+
+  // Test valid email
+  fireEvent.changeText(emailInput, "validemail@example.com");
+  expect(screen.queryByText("Email is invalid")).toBeFalsy();
+});
+
+test("inavlid email;", () => {
+  const store = mockStore({});
+
+  const { getByPlaceholderText, getByText } = render(
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  );
+  const emailInput = getByPlaceholderText("email");
+  fireEvent.changeText(emailInput, "invalid");
+  expect(screen.queryByText("Email is invalid")).toBeTruthy();
+});
+
+test("valid password length", () => {
+  const store = mockStore({});
+
+  const { getByPlaceholderText, queryByText } = render(
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  );
+
+  const passwordInput = getByPlaceholderText("password");
+
+  // Test valid password length
+  fireEvent.changeText(passwordInput, "password123");
+  expect(queryByText("Password is invalid")).toBeFalsy();
+});
+
+test("render forgot password link", () => {
+  const store = mockStore({});
+
+  const { getByText } = render(
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  );
+
+  const forgotPasswordLink = getByText("Forgot password?");
+
+  // Assert that the "Forgot password?" link is rendered
+  expect(forgotPasswordLink).toBeTruthy();
+});
